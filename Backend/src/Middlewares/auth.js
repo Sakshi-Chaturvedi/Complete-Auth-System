7f6bc6e48d17e224a0b20c02b882467b4fdf3804
Sakshi-Chaturvedi <sakshi3012002@gmail.com>
2026-02-13 19:59:18 +0530

    Make changes in isAuthenticated

--- Backend/src/Middlewares/auth.js
+++ Backend/src/Middlewares/auth.js
@@ -6,6 +6,9 @@ const user = require("../models/user.model");
 const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
 
+  console.log("Cookies:", req.cookies);
+  console.log("Token:", req.cookies.token);
+
   if (!token) {
     return next(new ErrorHandler("User is not Authenticated.", 400));
   }
