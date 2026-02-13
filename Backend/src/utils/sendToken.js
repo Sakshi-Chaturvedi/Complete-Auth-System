994cd71c80f7fa341393afda7e66d5ffd0f1fcef
Sakshi-Chaturvedi <sakshi3012002@gmail.com>
2026-02-13 19:48:59 +0530

    Changes in logout and login API

--- Backend/src/utils/sendToken.js
+++ Backend/src/utils/sendToken.js
@@ -5,9 +5,11 @@ const sendToken = (user, statusCode, message, res) => {
     .status(statusCode)
     .cookie("token", token, {
       expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
       ),
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
     })
     .json({
       success: true,
